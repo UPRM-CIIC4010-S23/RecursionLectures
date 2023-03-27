99e3719eaefadbd1f9d8f765a75468d697c3d8ca
andreschamorro <andres.chamorro@upr.edu>
2023-03-27 15:56:30 -0400

    Add Permutation and maze problem

---
+++ src/maze.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+/*
+ * A rat starts at a position and can only move in four directions:
+    - Right
+    - Down
+    - Left
+    - Up
+    The goal is to find the cheese.
+*/
+
+int maze[6][6] =
+{
+    {2, 2, 2, 0, 0, 0},
+    {0, 0, 2, 0, 0, 0},
+    {0, 0, 2, 2, 2, 0},
+    {0, 0, 2, 0, 3, 0},
+    {0, 0, 0, 0, 0, 0}
+};
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ src/permutation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+ * A permutation also called an 'arrangement number' is a rearrangement 
+ * of the elements of an string S into a one-to-one correspondence with 
+ * S itself.
+ * Input: S = "ABC"
+ * Output: "ABC", "ACB", "BAC", "BCA", "CBA", "CAB"
+*/
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
