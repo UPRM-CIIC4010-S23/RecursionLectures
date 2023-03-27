0b79db1c47b36b3e178e43844225f8e08c6e541f
Andres Chamorro <andres.chamorro@upr.edu>
2023-03-27 11:03:34 -0400

    Lecture 20.03.23

--- src/factorial.cpp
+++ src/factorial.cpp
@@ -5,12 +5,40 @@
  * to multiply all whole numbers from our 
  * chosen number down to 1.
  *
- * e.g. 5! = 5*4*3*2*1
+ * e.g. 5! = 5*4*3*2*1 = 1*2*3*4*5
+ * 5! = 5*4!
 */
+
+long iterFactial(long n) // Integer 64 bits
+{ 
+    long factorial = 1;
+    for (long i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+    
+}
+
+long recFactial(long n)
+{
+    if(n == 0) return 1;
+    return n * recFactial(n-1);
+}
 
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    /* code */
+    recFactial(5);
+    for (int k = 0; k < 20; k++)
+    {
+        cout << "IterFactorial(" << k << ") = " << iterFactial(k) << endl;
+    }
+
+    for (int k = 0; k < 20; k++)
+    {
+        cout << "RecFactorial(" << k << ") = " << recFactial(k) << endl;
+    }
+    
     return 0;
 }
--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -8,9 +8,45 @@
  * note: the input parameter is the position in the sequence
  */
 
+long recFibonacci(long n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return recFibonacci(n-1) + recFibonacci(n-2);
+}
+
+long iterFibonacci(long n)
+{
+    long f0 = 0;
+    long f1 = 1;
+    long fn;
+    for (long i = 2; i < n; i++)
+    {
+        fn = f0 + f1;
+        f0 = f1;
+        f1 = fn;
+    }
+    return fn;
+    
+}
+
+long iterFibonacciHelper(long f0, long f1, long n)
+{
+    if(n == 0) return f0;
+    return iterFibonacciHelper(f1, f0+f1, n-1);
+    
+}
+long iterFibonacci2(long n)
+{
+    return iterFibonacciHelper(0, 1, n);
+}
+
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    /* code */
+    for (int k = 0; k < 100; k++)
+    {
+        cout << "iterFibonacci(" << k << ") = " << iterFibonacci2(k) << endl;
+    }
     return 0;
 }
