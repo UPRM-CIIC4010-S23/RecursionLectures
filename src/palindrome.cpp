e759a3eb1b8af0105bd48501f5e67656fd097cc4
andreschamorro <andres.chamorro@upr.edu>
2023-03-27 21:54:15 -0400

    Palindrome and permutation lecture 27.03

--- src/palindrome.cpp
+++ src/palindrome.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 /*
  * A palindrome is a word, sentence, verse, or even number that reads 
@@ -11,8 +12,33 @@
  */
 
 using namespace std;
+
+bool iterIsPalindrome(string w)
+{
+    for (int i = 0; i < w.size()/2; i++)
+    {
+        if( w[i] != w[w.size() - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool recIsPalindrome(string w)
+{
+    if(w.size() < 2) return true;
+    if ( w[0] == w[w.size() -1])
+    {
+        return recIsPalindrome(w.substr(1, w.size() -2));
+    }
+    return false;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    cout << boolalpha;
+    cout << iterIsPalindrome("radar") << endl;
+    cout << recIsPalindrome("saippuakivikauppias") << endl;
     return 0;
 }
--- src/permutation.cpp
+++ src/permutation.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 /*
  * A permutation also called an 'arrangement number' is a rearrangement 
@@ -9,8 +11,36 @@
 */
 
 using namespace std;
+
+vector<string> recPermutation(string w)
+{
+    vector<string> result;
+    if (w.size() < 2)
+    {
+        result.push_back(w);
+        return result;
+    }
+    
+    for (int i = 0; i < w.size(); i++)
+    {
+        string currentLetter = w.substr(i, 1);
+        string shorterString = w.substr(0, i) + w.substr(i+1, w.size() -i -1);
+        vector<string> shorterPermutation = recPermutation(shorterString);
+        for (int i = 0; i < shorterPermutation.size(); i++)
+        {
+            result.push_back(currentLetter + shorterPermutation[i]);
+        }
+    }
+    
+    return result;
+}
 int main(int argc, char const *argv[])
 {
-    /* code */
+    vector<string> permutation = recPermutation("abc");
+    for (int i = 0; i < permutation.size(); i++)
+    {
+        cout << permutation[i] << endl;
+    }
+    
     return 0;
 }
